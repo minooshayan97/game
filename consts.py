14e8ddf496740d193b1afc21f5c90a025727fe8a
minoo_shayan <minooshayan97@gmail.com>
2018-08-01 00:05:49 +0430

    refactored definition of constants in consts.py

---
+++ consts.py
@@ -0,0 +1,15 @@
+COLORS = {
+    "1": "blue",
+    "2": "red",
+    "3": "green",
+    "4": "yellow",
+    "5": "purple",
+    "6": "orange",
+    "7": "pink",
+    "8": "white",
+    "9": "black",
+    "10": "brown"
+}
+GAME_LENGTH = 4
+MIN_COLORS = int(min(COLORS.keys()))
+MAX_COLORS = int(max(COLORS.keys()))
